0ec653e941cc21fd2e1270276f847ceadde18683
Archi903 <artpele@yandex.ru>
2022-12-12 13:07:14 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+    }
+    public static void task1 () {
+        System.out.println("Задание 1");
+        int [] number = new int [3];
+        number [0] = 1;
+        number [1] = 2;
+        number [2] = 3;
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+
+
     }
 }
