6cefe340198e689af44bfd1b01d8554a0d71c62b
Archi903 <artpele@yandex.ru>
2022-12-12 13:11:37 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
 
     }
-    public static void task1 () {
+
+    public static void task1() {
         System.out.println("Задание 1");
-        int [] number = new int [3];
-        number [0] = 1;
-        number [1] = 2;
-        number [2] = 3;
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        System.out.println(number[0]);
+        System.out.println(number[1]);
+        System.out.println(number[2]);
+
+
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+    double [] number = {1.57, 7.654, 9.986};
         System.out.println(number[0]);
         System.out.println(number[1]);
         System.out.println(number[2]);
