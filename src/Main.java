038af0fe62295446d3d4eab5bb58bf01d4d6b680
Archi903 <artpele@yandex.ru>
2022-12-12 13:17:06 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
     }
 
@@ -15,16 +16,30 @@ public class Main {
         System.out.println(number[1]);
         System.out.println(number[2]);
 
+        double [] numberFraction = {1.57, 7.654, 9.986};
+        System.out.println(numberFraction[0]);
+        System.out.println(numberFraction[1]);
+        System.out.println(numberFraction[2]);
+
+        int [] myNumber = {5, 10, 15, 20 ,25};
+        System.out.println(myNumber[0]);
+        System.out.println(myNumber[1]);
+        System.out.println(myNumber[2]);
+        System.out.println(myNumber[3]);
+        System.out.println(myNumber[4]);
+
+
 
     }
 
     public static void task2() {
         System.out.println("Задание 2");
-    double [] number = {1.57, 7.654, 9.986};
-        System.out.println(number[0]);
-        System.out.println(number[1]);
-        System.out.println(number[2]);
 
+
+
+    }
+    public static void task3() {
+        System.out.println("Задание 3");
 
     }
 }
